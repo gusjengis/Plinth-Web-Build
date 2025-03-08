14c84d06fe0279ce3e9ee3b8d2c87bd3c735479e
gusjengis <anthony.j.green@outlook.com>
2025-03-08 11:38:41 -0800

    added flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default =
+        pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            nodejs_23
+          ];
+        };
+    };
+}
